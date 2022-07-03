829b04097e417781625115f692da18bb3a83832a
Mull-Algibrhani <diazmizkot@gmail.com>
2022-07-03 23:28:14 +0800

    menambahkan tutorial RETS API

---
+++ testapi.py
@@ -0,0 +1,17 @@
+import requests
+import json
+api_url = "https://www.emsifa.com/api-wilayah-indonesia/api/regencies/73.json"
+response = requests.get(api_url)
+print("=====DATA KABUPATEN======")
+if response.status_code == 200:
+    data = response.json()
+    for d in data:
+        id = d[0]
+        prov_id = d['province_id']
+        name = d['name']
+        print(f"""
+ID: {id}
+PROV ID : {prov_id}
+Nama Kab/Kota : {name}""")
+else:
+    print("error")
