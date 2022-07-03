da6f5d1ab1c0397069fa2ed0193839f977c6ade1
Mull-Algibrhani <diazmizkot@gmail.com>
2022-07-03 14:29:16 +0800

    modified:   21_latiha_operasi_string.py

--- 21_latiha_operasi_string.py
+++ 21_latiha_operasi_string.py
@@ -12,3 +12,8 @@ print(txt.encode(encoding="ascii", errors="ignore"))
 print(txt.encode(encoding="ascii", errors="namereplace"))
 print(txt.encode(encoding="ascii", errors="replace"))
 print(txt.encode(encoding="ascii", errors="xmlcharrefreplace"))
+
+# melakukan pencarian index pada string
+txt = "Hello, welcome to my world."
+x = txt.find("to")
+print(x)
