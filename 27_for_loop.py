767cedb1635f9a34e0bf3ae6e412bb26a9779d44
Mull-Algibrhani <diazmizkot@gmail.com>
2022-07-11 22:36:54 +0800

    Menambahkan Tutorial For Loop

---
+++ 27_for_loop.py
@@ -0,0 +1,20 @@
+print(5*"="+" FOR LOOP "+5*"=")
+
+angka = [1, 3, 5, 7]
+for i in angka:
+    print(f"loop : {i}")
+
+print("\n")
+nama = ["Mull", "Leni", "Akbar", "Risna"]
+for nama_nama in nama:
+    print(f"Nama Ke : {nama_nama}")
+
+print("\n")
+angkarange = range(10)
+for ranges in angkarange:
+    print(f"Loop Ke : {ranges}")
+
+print("\n")
+data_string = "Mull Algibrhani"
+for data_str in data_string:
+    print(data_str)
